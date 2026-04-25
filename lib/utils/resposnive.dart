f9772aef84b7bdeeb21f0f9e0c0d30da4d6cd283
Habeeb vk <habeebvk11@gmail.com>
2026-04-25 09:28:06 +0530

    Initial commit

---
+++ lib/utils/resposnive.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Responsive {
+  // Breakpoints
+  static bool isMobile(BuildContext context) =>
+      MediaQuery.of(context).size.width < 600;
+
+  static bool isTablet(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 600 &&
+      MediaQuery.of(context).size.width < 1024;
+
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 1024;
+
+  // Screen dimensions
+  static double screenWidth(BuildContext context) =>
+      MediaQuery.of(context).size.width;
+
+  static double screenHeight(BuildContext context) =>
+      MediaQuery.of(context).size.height;
+}
---
+++ lib/view/onboarding.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:svg_flutter/svg.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:totalxproject/utils/resposnive.dart';
+
+class OnBoardingScreen extends StatefulWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       backgroundColor: Color(0xffFAFAFA),
+       body: Container(
+        width: Responsive.isMobile(context) ? 600 : 800,
+        height: Responsive.isMobile(context) ? 1000 : 1000,
+          child: Column(
+            children: [
+            SizedBox(height: 50.h,),
+            Padding(
+              padding: EdgeInsets.only(right:45.r),
+              child: Text("Login or create an account",style: GoogleFonts.montserrat(fontWeight: FontWeight.w600,fontSize: 18.sp),),
+              ),
+              SizedBox(height: 60.h),
+              Text(
+                "Welcome\nTOTAL-X",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.montserrat(
+                  fontWeight: FontWeight.w700,
+                  color: const Color(0xff05609A),
+                  fontSize: 28.sp,
+                ),
+              ),
+              SizedBox(height: 40.h,),
+              Center(
+                child: Image.asset(
+                  "assets/OBJECTS.png",
+                ),
+              ),
+              SizedBox(height: 100.h),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.w),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:Color(0xffFAFAFA),
+                    side: BorderSide(
+                        color: Color(0xff00000066),
+                        width: 1.w
+                      ),
+                    minimumSize: Size(300, 50.h),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.r),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SvgPicture.asset(
+                        "assets/google.svg",
+                        width: 24.w,
+                        height: 24.w,
+                      ),
+                      SizedBox(width: 12.w),
+                      Text(
+                        "Continue with Google",
+                        style: GoogleFonts.inter(
+                          fontSize: 16.sp,
+                          color: Colors.black
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 10.h),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: RichText(
+                text: TextSpan(
+                  style: GoogleFonts.montserrat(
+                    color: Colors.black, // default text color
+                  ),
+                  children: [
+                    const TextSpan(text: "By Continuing, I Agree to TotalX's "),
+                    
+                    TextSpan(
+                      text: "Terms and Conditions",
+                      style: GoogleFonts.montserrat(
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline, // optional
+                      ),
+                    ),
+
+                    const TextSpan(text: " & "),
+
+                    TextSpan(
+                      text: "Privacy Policy",
+                      style: GoogleFonts.montserrat(
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline, // optional
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              )
+            ],
+          ),
+        ),
+      );
+  }
+}
